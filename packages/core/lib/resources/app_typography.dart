8dbd6c34a76c016af050b8760a36b1c508a0e2a4
dimas <dimasnur.dev@gmail.com>
2024-02-03 05:58:07 +0700

    ✨ feat: disable inhereted text style in typography

--- packages/core/lib/resources/app_typography.dart
+++ packages/core/lib/resources/app_typography.dart
@@ -7,28 +7,31 @@ import 'app_colors.dart';
 class AppTypography {
   /// Sub Heading/Large - H5
   static TextStyle h5 = TextStyle(
+    inherit: false,
     fontFamily: GoogleFonts.poppins().fontFamily,
     fontSize: 16.sp,
     fontWeight: FontWeight.w700,
     height: 1.25,
-    color: AppColors.textWhite,
+    color: AppColors.textDark,
   );
 
   /// Body Formatting/Small
   static TextStyle bodySmall = TextStyle(
+    inherit: false,
     fontFamily: GoogleFonts.poppins().fontFamily,
     fontSize: 11.sp,
     fontWeight: FontWeight.w500,
     height: 1.45,
-    color: AppColors.textWhite,
+    color: AppColors.textDark,
   );
 
   /// Body Formatting/Xtra Small
   static TextStyle bodyXSmall = TextStyle(
+    inherit: false,
     fontFamily: GoogleFonts.poppins().fontFamily,
     fontSize: 8.sp,
     fontWeight: FontWeight.w500,
     height: 1.5,
-    color: AppColors.textWhite,
+    color: AppColors.textDark,
   );
 }
