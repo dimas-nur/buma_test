b31dcca2832a53ddeaba4d0d0e9e10b7e49b2ddd
dimas <dimasnur.dev@gmail.com>
2024-02-02 09:08:53 +0700

    ✨ feat: add flutter_screenutil and set design size in buma hris app

--- apps/buma_hris/lib/app/buma_hris_app.dart
+++ apps/buma_hris/lib/app/buma_hris_app.dart
@@ -1,24 +1,32 @@
 import 'package:components/banner/flavor_banner.dart';
 import 'package:core/core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BumaHrisApp extends StatelessWidget {
   const BumaHrisApp({super.key});
 
+  static const Size _designSize = Size(393, 852);
+
   @override
   Widget build(BuildContext context) {
-    return FlavorBanner(
-      color: FlavorConfig.instance.color,
-      message: FlavorConfig.instance.name.toUpperCase(),
-      isProduction: FlavorConfig.isProduction,
-      child: MaterialApp(
-        title: FlavorConfig.instance.values.appName,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-          useMaterial3: true,
+    return ScreenUtilInit(
+      designSize: _designSize,
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: FlavorBanner(
+        color: FlavorConfig.instance.color,
+        message: FlavorConfig.instance.name.toUpperCase(),
+        isProduction: FlavorConfig.isProduction,
+        child: MaterialApp(
+          title: FlavorConfig.instance.values.appName,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+            useMaterial3: true,
+          ),
+          debugShowCheckedModeBanner: false,
+          home: MyHomePage(title: FlavorConfig.instance.values.appName),
         ),
-        debugShowCheckedModeBanner: false,
-        home: MyHomePage(title: FlavorConfig.instance.values.appName),
       ),
     );
   }
